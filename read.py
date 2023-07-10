32a1ce40070a82892386264569f9ef981954fc6c
fishfreetaken <471088280@qq.com>
2023-07-10 23:56:38 +0800

    --story=增加mean

--- read.py
+++ read.py
@@ -64,7 +64,7 @@ def IterAllgo(interval , averageStep):
     for i in range (1,si):
         if len(preaverage) >= averageStep :
             del preaverage[0]
-        preaverage.append(data['Open'][i]  - data['Close'][i])
+        preaverage.append(data['Open'][i-1]  - data['Close'][i-1])
         if len(preaverage) < averageStep :
             continue
         
@@ -98,5 +98,52 @@ def IterAllgo(interval , averageStep):
 
 print(data)
 
-for i in range(1,30):
-    IterAllgo(i,7)
+def CpValue2PreValue(i):
+    preValue = data['Open'][i] - (data['High'][i-1]-data['Low'][i-1])*2/3
+    if preValue < data['Low'][i]:
+        preValue = data['Close'][i]
+    return preValue
+def CpValue2SaleValue(i):
+    saleprivace = data['Open'][i] + (data['High'][i-1]-data['Low'][i-1])*2/3 
+    if saleprivace > data['High'][i]:
+        saleprivace =  data['Close'][i]
+    return saleprivace
+
+def StaticInfo():
+    si = data['Date'].size
+    ratearry=[]
+    for idx,raw in data.iterrows():
+        rate = (raw['Close'] - raw['Open'] )*100/ raw['Open']
+        if rate <0 :
+            rate = 0 -rate 
+        ratearry.append(rate)
+    lt= pd.Series(ratearry)
+    print('average amplitute :%f fx:%f' % (lt.mean(),lt.var()))
+    avermean = lt.mean()
+    preValue = 0 
+    #假如我超过了平均的增值振幅我就买，超过平均跌幅我就卖
+    sum=0
+    mincnt=0
+    bigcnt=0
+
+    begindate=""
+    for i in range (1,si):
+        rate = (data['Close'][i-1] - data['Open'][i-1] )*100/ data['Open'][i-1]
+        if preValue == 0 and rate > 0 and rate >=  avermean:
+            preValue = data['Open'][i]
+            begindate=data['Date'][i]
+        if preValue > 0 and rate < 0 and (0-rate) >= avermean: 
+            saleprivace = data['Open'][i]
+            sum += saleprivace - preValue
+            if saleprivace - preValue < 0 :
+                mincnt+=1
+            else :
+                bigcnt+=1
+            print("begin:%s end:%s diff:%f rate:%f  sale:%f prevalue:%f"%(begindate,data['Date'][i],saleprivace - preValue, rate,saleprivace,preValue))
+            preValue=0
+    
+    print("sum:%f mincnt:%d bigcnt:%d"%(sum,mincnt,bigcnt))
+
+StaticInfo()
+#for i in range(1,30):
+#    IterAllgo(i,7)
